655852145ddc73984f3b9cf0c57b27b14eb286d2
Eduard <oplakanskiytitan@gmail.com>
2024-02-02 21:58:00 +0100

    new tables added

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import django_setup
-from myapp.models import Teacher, Subject, Student, Class
+from myapp.models import Teacher, Subject, Student, Class, WeeklySchedule, DailySchedule
+from rich.console import Console
+from rich.table import Table
 
 def add_class(name):
-    Class.objects.create(name = name)
+    default = get_subject_by_name("no lesson")
+    plan = WeeklySchedule(
+        monday = DailySchedule(default).save(),
+        tuesday = DailySchedule(default).save(),
+        wednesday = DailySchedule(default).save(),
+        thursday = DailySchedule(default).save(),
+        friday = DailySchedule(default).save()
+    )
+    the_class = Class(name = name, schedule = plan)
+
+    plan.save()
+    the_class.save()
 
 def get_class_by_name(name):
     return Class.objects.get(name = name)
@@ -12,6 +25,18 @@ def add_student(name, surname, the_class):
         name = name,
         surname = surname, 
         student_class = get_class_by_name(the_class)
+    )
+
+def get_teacher_by_name(name):
+    return Teacher.objects.get(name = name)
+
+def get_student_by_id(id):
+    return Student.objects.get(id = id)
+
+def add_subject(name, teacher_name):
+    Subject(
+        name = name,
+        teacher = get_teacher_by_name(teacher_name)
     )
 
 def get_students_in_class(class_name):
@@ -36,7 +61,73 @@ def get_teachers_info():
 def get_subjects_of_teacher(teachers_id):
     return Subject.objects.filter(teacher = teachers_id)
 
+def get_class_by_name(class_name):
+    return Class.objects.get(name = class_name)
+
+def get_classes_plan(class_name):
+    classs = get_class_by_name(class_name)
+    return WeeklySchedule.objects.get(related_class = classs.id)
+
+def add_lesson_to_plan(class_name, day, lesson_number, subject):
+    plan = get_classes_plan(class_name)
+
+    if day == 'monday':
+        day = plan.monday
+    elif day == 'tuesday':
+        day = plan.tuesday
+    elif day == 'wednesday':
+        day = plan.wednesday
+    elif day == 'thirsday':
+        day = plan.thursday
+    elif day == 'friday':
+        day = plan.friday
+
+    if lesson_number == 1:
+        day.first_lesson = get_subject_by_name(subject)
+    elif lesson_number == 2:
+        day.second_lesson = get_subject_by_name(subject)
+    elif lesson_number == 3:
+        day.third_lesson = get_subject_by_name(subject)
+    elif lesson_number == 4:
+        day.fourth_lesson = get_subject_by_name(subject)
+    elif lesson_number == 5:
+        day.fifth_lesson = get_subject_by_name(subject)
+    elif lesson_number == 6:
+        day.sixth_lesson = get_subject_by_name(subject)
+    elif lesson_number == 7:
+        day.seventh_lesson = get_subject_by_name(subject)
+
+def see_classes_plan(class_name):
+    table = Table(title=class_name)
+    columns = ['№', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+    for column in columns:
+        table.add_column(column)
+
+    week = WeeklySchedule.objects.get(related_class = get_class_by_name(class_name).id)
+    for number, day in enumerate(week, 1):
+        row = [number]
+
+        for subject in day:
+            row.append(subject.name)
+        
+        table.add_row(row)
+
+    Console().print(table)
+
 #------------------------------------------------------------------------------------
+
 run = True
+question = '''
+Welcome to our school!
+Pick one of the following actions
+1. Log in
+2. Add a student
+3. Add a teacher 
+4. Manage schedule
+5. 
+6.
+7.
+8.
+'''
 while run:
-    pass
+    print(question)
--- myapp/models.py
+++ myapp/models.py
@@ -8,7 +8,8 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.surname
-    
+
+
 class Subject(models.Model):
     name = models.CharField(max_length = 30, unique = True)
     teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING, related_name='subjects')
@@ -26,8 +27,25 @@ class Student(models.Model):
     def __str__(self):
         return self.surname
 
-
 class Class(models.Model):
     name = models.CharField(max_length = 30, unique = True)
+    schedule = models.OneToOneField('WeeklySchedule', on_delete = models.DO_NOTHING, related_name = 'related_class')
 
+    def __str__(self):
+        return self.name
 
+class WeeklySchedule(models.Model):
+    monday = models.OneToOneField('DailySchedule', on_delete = models.DO_NOTHING, related_name = 'related_schedule')
+    tuesday = models.OneToOneField('DailySchedule', on_delete = models.DO_NOTHING, related_name = 'related_schedule')
+    wednesday = models.OneToOneField('DailySchedule', on_delete = models.DO_NOTHING, related_name = 'related_schedule')
+    thursday= models.OneToOneField('DailySchedule', on_delete = models.DO_NOTHING, related_name = 'related_schedule')
+    friday = models.OneToOneField('DailySchedule', on_delete = models.DO_NOTHING, related_name = 'related_schedule')
+
+class DailySchedule(models.Model):
+    first_lesson = models.ForeignKey(Subject, related_name = "lessons", on_delete = models.DO_NOTHING)
+    second_lesson = models.ForeignKey(Subject, related_name = "lessons", on_delete = models.DO_NOTHING)
+    third_lesson = models.ForeignKey(Subject, related_name = "lessons", on_delete = models.DO_NOTHING)
+    fourth_lesson = models.ForeignKey(Subject, related_name = "lessons", on_delete = models.DO_NOTHING)
+    fifth_lesson = models.ForeignKey(Subject, related_name = "lessons", on_delete = models.DO_NOTHING)
+    sixth_lesson = models.ForeignKey(Subject, related_name = "lessons", on_delete = models.DO_NOTHING)
+    seventh_lesson = models.ForeignKey(Subject, related_name = "lessons", on_delete = models.DO_NOTHING)
